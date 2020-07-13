d17190d53ccdb2a3e196f5e5c1332a5d4fb15af9
Hong Yeongpyo <cjsgkdudvy01@naver.com>
2020-07-13 23:30:24 +0900

    2nd

--- src/main/java/hong/yeongpyo/healthCare/controller/MainController.java
+++ src/main/java/hong/yeongpyo/healthCare/controller/MainController.java
@@ -1,5 +1,18 @@
 package hong.yeongpyo.healthCare.controller;
 
-public class MainController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+public class MainController {
+	@RequestMapping("/main")
+	public String main() {
+		
+		return "main";
+	}
+	@RequestMapping("/login.html")
+	public String mainasd() {
+		
+		return "login.html";
+	}
 }

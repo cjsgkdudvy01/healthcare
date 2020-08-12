496fd70ac8c4a780bcea986877862779d268b265
Hong Yeongpyo <cjsgkdudvy01@naver.com>
2020-08-12 23:56:36 +0900

    version-1.0

---
+++ src/main/java/hong/yeongpyo/healthCare/springSecurity/Member.java
@@ -0,0 +1,158 @@
+package hong.yeongpyo.healthCare.springSecurity;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.SpringSecurityCoreVersion;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.util.Assert;
+
+public class Member implements UserDetails{
+	private String id;
+	private String password;
+	private String name;
+	private int phonenum1;
+	private int phonenum2;
+	private int phonenum3;
+	private boolean gender;
+	private Set<GrantedAuthority> authorities;
+	
+	public Member(String id, String password, String name, int phonenum1, int phonenum2, int phonenum3, boolean gender,
+			Set<GrantedAuthority> authorities) {
+		super();
+		this.id = id;
+		this.password = password;
+		this.name = name;
+		this.phonenum1 = phonenum1;
+		this.phonenum2 = phonenum2;
+		this.phonenum3 = phonenum3;
+		this.gender = gender;
+		this.authorities = authorities;
+	}
+	
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getPhonenum1() {
+		return phonenum1;
+	}
+
+	public void setPhonenum1(int phonenum1) {
+		this.phonenum1 = phonenum1;
+	}
+
+	public int getPhonenum2() {
+		return phonenum2;
+	}
+
+	public void setPhonenum2(int phonenum2) {
+		this.phonenum2 = phonenum2;
+	}
+
+	public int getPhonenum3() {
+		return phonenum3;
+	}
+
+	public void setPhonenum3(int phonenum3) {
+		this.phonenum3 = phonenum3;
+	}
+
+	public boolean isGender() {
+		return gender;
+	}
+
+	public void setGender(boolean gender) {
+		this.gender = gender;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setAuthorities(Set<GrantedAuthority> authorities) {
+		this.authorities = authorities;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	} 
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	private static SortedSet<GrantedAuthority> sortAuthorities(Collection<? extends GrantedAuthority> authorities) {
+        Assert.notNull(authorities, "Cannot pass a null GrantedAuthority collection");
+        // Ensure array iteration order is predictable (as per UserDetails.getAuthorities() contract and SEC-717)
+        SortedSet<GrantedAuthority> sortedAuthorities =
+            new TreeSet<GrantedAuthority>(new AuthorityComparator());
+
+        for (GrantedAuthority grantedAuthority : authorities) {
+            Assert.notNull(grantedAuthority, "GrantedAuthority list cannot contain any null elements");
+            sortedAuthorities.add(grantedAuthority);
+        }
+        return sortedAuthorities;
+	}
+	private static class AuthorityComparator implements Comparator<GrantedAuthority>, Serializable {
+        private static final long serialVersionUID = SpringSecurityCoreVersion.SERIAL_VERSION_UID;
+
+        public int compare(GrantedAuthority g1, GrantedAuthority g2) {
+            // Neither should ever be null as each entry is checked before adding it to the set.
+            // If the authority is null, it is a custom authority and should precede others.
+            if (g2.getAuthority() == null) {
+                return -1;
+            }
+
+            if (g1.getAuthority() == null) {
+                return 1;
+            }
+
+            return g1.getAuthority().compareTo(g2.getAuthority());
+        }
+    }
+}
